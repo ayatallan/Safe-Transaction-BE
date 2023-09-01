4dda8d1b9db2460c257aed02e5bbe2a21affc956
Mohammed Jawareesh-IT <100040175+Mohammad-jawareesh-PS@users.noreply.github.com>
2023-09-02 01:50:56 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import transactionsRouter from './routers/transaction.js'; 
 import cors from 'cors';
+import predictFraudRouter from './predict-fraud.js';
 
 const app = express();
 app.use(cors());
-
 app.use(express.json());
 
 const MONGO_URL = 'mongodb://127.0.0.1:27017/safe-transactions';
@@ -18,7 +17,7 @@ mongoose.connect(MONGO_URL)
         console.error('Error connecting to MongoDB:', error);
     });
 
-app.use('/transaction', transactionsRouter);
+app.use('/predict', predictFraudRouter);
 
 const port = 3002;
 

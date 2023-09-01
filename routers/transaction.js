c0cc5c7ce4ae0fb7cdea2c0012e63d414cb8009d
MohammadJawareeshOyoun <122981477+MohammadJawareesh@users.noreply.github.com>
2023-09-02 02:19:52 +0300

    Update transaction.js

--- routers/transaction.js
+++ routers/transaction.js
@@ -1,64 +1,58 @@
+
 import express from 'express';
-import Transaction from '../model/transaction.js';
+import mongoose from 'mongoose';
+import Transaction from './transaction.js';
 import * as tf from '@tensorflow/tfjs-node';
 import fs from 'fs';
 
-// Define the model and threshold loading function
-// async function loadModelAndThreshold() {
-//   const modelPath = 'C:/Users/pc/Desktop/ST-BE/autoencoder_model.h5';
-//   const loadedModel = await tf.loadLayersModel(`file://${modelPath}`);
-//   console.log("heloooooooooooooooooo");
-  
-//   const thresholdFilePath = 'C:/Users/pc/Desktop/ST-BE/threshold.txt';
-//   const threshold = parseFloat(fs.readFileSync(thresholdFilePath, 'utf-8'));
-  
-//   return { model: loadedModel, threshold };
-// }
+const app = express();
+app.use(express.json());
+
 async function loadModel() {
   const modelPath = 'C:/Users/pc/Desktop/ST-BE/autoencoder_model.h5';
   const model = await tf.loadLayersModel(`file://${modelPath}`);
-  return model;
+  
+  const thresholdFilePath = 'C:/Users/pc/Desktop/ST-BE/threshold.txt';
+  const threshold = parseFloat(fs.readFileSync(thresholdFilePath, 'utf-8'));
+  
+  return { model, threshold };
 }
 
-const router = express.Router();
-router.post('/fraud', async (req, res) => {
+app.post('/fraud', async (req, res) => {
   try {
-    console.log('Received Request Body:', req.body); // Log the received body
-    
     const { model, threshold } = await loadModel();
-    console.log("test")
 
-    // Use feature values directly from the request body
-    const features = {
-      "Card Type": req.body.cardType,
-      "Channel": req.body.channel,
-      "Transaction Type": req.body.transactionType,
-      "Transaction Type Group": req.body.transactionTypeGroup,
-      "Entry Mode": req.body.entryMode,
-      "Outlet ID": req.body.outletId,
-      "Merchant Country": req.body.merchantCountry,
-      "Merchant Activity": req.body.merchantActivity,
-      "Client Code": req.body.clientCode,
-      "Amount USD": req.body.amountUSD,
-      // ... Add more feature values here ...
-    };
-    console.log('features:', features); // Log the received body
+    const transactionData = await Transaction.findOne({ _id: req.body.transactionId }); 
 
-    // Prepare input data as tensor
-    const inputData = tf.tensor2d(features);
+    if (!transactionData) {
+      return res.status(404).json({ message: 'Transaction not found' });
+    }
 
-    // Perform prediction
+    const inputData = tf.tensor2d([[
+      transactionData.cardType,
+      transactionData.channel,
+      transactionData.transactionType,
+      transactionData.transactionTypeGroup,
+      transactionData.entryMode,
+      transactionData.outletId,
+      transactionData.merchantCountry,
+      transactionData.merchantActivity,
+      transactionData.clientCode,
+      transactionData.amountUSD,
+    ]]);
+
     const prediction = model.predict(inputData);
-    const predictionValue = prediction.dataSync(); // Get prediction value
+    const predictionValue = prediction.dataSync()[0]; 
 
-    const isFraudulent = predictionValue > threshold; res.json({ isFraudulent });
+    const isFraudulent = predictionValue > threshold;
+    res.json({ isFraudulent });
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Something went wrong' });
   }
 });
 
-
-
-
-
-export default router;
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
